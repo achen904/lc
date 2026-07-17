29f8eb6b8d37c4b1a25d265e86753b79bd9ef7b0
Andy Chen <andychen904@yahoo.com>
2026-07-17 01:55:47 -0400

    Add: house-robber - submission-1

---
+++ Data Structures & Algorithms/house-robber/submission-1.py
@@ -0,0 +1,16 @@
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        #another space optimized DP problem
+        #At a given house i, the max amount of money we can have
+        #is max(max(house i - 1), max(house i - 2) + nums[i])
+        #we return the max at the last house
+
+        #base case - max money at first house is robbing it
+        oneBack = nums[0]
+        twoBack = 0
+        for i in range(1, len(nums)):
+            curMax = max(nums[i] + twoBack, oneBack)
+            twoBack = oneBack
+            oneBack = curMax
+        #at the end oneBack is at house(len(nums)) which gives max at the end of the neighborhood
+        return oneBack

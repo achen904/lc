393330e593632fecace7c7ee5ea982f7b6e14444
Andy Chen <andychen904@yahoo.com>
2026-07-23 01:58:44 -0400

    Add: count-paths - submission-3

---
+++ Data Structures & Algorithms/count-paths/submission-3.py
@@ -0,0 +1,10 @@
+class Solution:
+    def uniquePaths(self, m: int, n: int) -> int:
+        dp = [1] * n
+
+        for i in range(1, m ):
+            temp = [1] * n
+            for j in range(1, n):
+                temp[j] = temp[j - 1] + dp[j]
+            dp = temp
+        return dp[n - 1]
